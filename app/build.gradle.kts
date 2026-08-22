1c295fc0d9b859dd441c7700968aebb7837ecc36
vnnit <u8802880@gmail.com>
2026-08-22 11:49:50 +0700

    release: HHKungfu TV v2.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.hhkungfu.tv"
         minSdk = 24
         targetSdk = 34
-        versionCode = 13
-        versionName = "2.1.1"
+        versionCode = 14
+        versionName = "2.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
